76e757055d56dc938fde314798708471f586acb1
Santiago Vasquez <santiagovasquez2711@gmail.com>
2025-07-11 03:15:37 +0200

    login

--- configuracion/settings.py
+++ configuracion/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = '/inicio/'
+LOGIN_REDIRECT_URL = '/dashboard/'
 
 LOGOUT_REDIRECT_URL = '/login/'
 
