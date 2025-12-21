afb3c284979984fd306d13954f295cb50f21eaf7
santiago <santiagovasquez2711@gmail.com>
2025-12-20 21:14:10 -0500

    balance

--- configuracion/settings.py
+++ configuracion/settings.py
@@ -30,7 +30,7 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', 'dpg-d53ji2ruibrs73fu2ts0-a']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', 'https://inventario-o9gf.onrender.com']
 
 
 # Application definition
