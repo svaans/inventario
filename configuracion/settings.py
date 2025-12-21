595e23fcb3f7e0d982691ac3392038d7f8410d92
santiago <santiagovasquez2711@gmail.com>
2025-12-20 23:32:03 -0500

    balance

--- configuracion/settings.py
+++ configuracion/settings.py
@@ -86,10 +86,21 @@ WSGI_APPLICATION = 'configuracion.wsgi.application'
 
  # asegúrate que esté al principio del archivo
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
