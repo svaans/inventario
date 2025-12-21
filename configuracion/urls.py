e40725f88fa8d8436c1c35fe10e31d317c36d3ec
santiago <santiagovasquez2711@gmail.com>
2025-12-21 05:23:09 -0500

    balance

--- configuracion/urls.py
+++ configuracion/urls.py
@@ -16,13 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import redirect
-
-def root_redirect(request):
-    return redirect("/login/")
 
 urlpatterns = [
-    path("", root_redirect),
     path("admin/", admin.site.urls),
     path("", include("core.urls")),
 ]
